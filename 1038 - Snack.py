ee041aa8cf81f5eb0b7eca4d2349f6a2375d9f0c
SanzidaRitu <mail.sanzida@gmail.com>
2025-08-28 21:21:49 +0600

    Added Problem No 1038

---
+++ 1038 - Snack.py
@@ -0,0 +1,15 @@
+X, Y = map(int, input().split())
+
+if X == 1:
+    price = 4.00
+elif X == 2:
+    price = 4.50
+elif X == 3:
+    price = 5.00
+elif X == 4:
+    price = 2.00
+elif X == 5:
+    price = 1.50
+
+total = price * Y
+print(f'Total: R$ { total:.2f}')
