db5516494bb0d82c9a8b8c99559b8c17521799f1
SanzidaRitu <mail.sanzida@gmail.com>
2025-09-01 17:00:23 +0600

    Added Problem No 1066

---
+++ 1066 - Even, Odd, Positive and Negative.py
@@ -0,0 +1,17 @@
+even = odd = pos = neg =0
+
+for i in range(5):
+    n = int(input())
+    if n % 2 == 0:
+        even += 1
+    else:
+        odd += 1
+    if n > 0:
+        pos += 1
+    elif n < 0:
+        neg += 1
+
+print(f'{even} valor(es) par(es)')
+print(f'{odd} valor(es) impar(es)')
+print(f'{pos} valor(es) positivo(s)')
+print(f'{neg} valor(es) negativo(s)')
