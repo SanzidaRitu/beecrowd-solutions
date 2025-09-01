054dcf280621bcb273f927034cfa27b35e13d93d
SanzidaRitu <mail.sanzida@gmail.com>
2025-09-01 16:46:18 +0600

    Added Problem No 1064

---
+++ 1064 - Positives and Average.py
@@ -0,0 +1,11 @@
+count = 0
+total = 0.0
+
+for i in range(6):
+    n = float(input())
+    if n > 0:
+        count += 1
+        total += n
+
+print(f'{count}' ' valores positivos')
+print(f'{total / count:.1f}')
