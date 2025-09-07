1155855e90098ca9aaa5d42ffe521811d217e5be
SanzidaRitu <mail.sanzida@gmail.com>
2025-09-07 20:10:13 +0600

    Added Problem No 1134

---
+++ 1134 - Type of Fuel.py
@@ -0,0 +1,18 @@
+alcohol = gasoline = diesel = 0
+
+while True:
+    code = int(input())
+
+    if code == 4:
+        break
+    elif code == 1:
+        alcohol += 1
+    elif code == 2:
+        gasoline += 1
+    elif code == 3:
+        diesel += 1
+
+print("MUITO OBRIGADO")
+print(f"Alcool: {alcohol}")
+print(f"Gasolina: {gasoline}")
+print(f"Diesel: {diesel}")
