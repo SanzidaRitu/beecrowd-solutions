def44f9d48b45cd40590871646fd3685d7b788c3
SanzidaRitu <mail.sanzida@gmail.com>
2025-09-08 19:50:48 +0600

    Updated Problem No 1151

--- 1151 - Easy Fibonacci.py
+++ 1151 - Easy Fibonacci.py
@@ -1,10 +1,13 @@
 N = int(input())
-a, b = 0, 1
+a = 0
+b = 1
 
 for i in range(N):
     if i == N - 1:
         print(a)
     else:
-        print(a, end = ' ')
+        print(a, end=' ')
 
-    a, b = b, a + b
+    c = a + b
+    a = b
+    b = c
