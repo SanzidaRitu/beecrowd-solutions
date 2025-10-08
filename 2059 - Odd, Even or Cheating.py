efec383210cd06ea3f98f29f732aa27ba41fb8b9
SanzidaRitu <mail.sanzida@gmail.com>
2025-10-08 16:40:32 +0600

    Added Problem No 2059

---
+++ 2059 - Odd, Even or Cheating.py
@@ -0,0 +1,23 @@
+p, j1, j2, r, a = map(int,input().split())
+
+result = j1 + j2
+
+if r == 1 and a == 1:
+    print("Jogador 2 ganha!")
+
+elif r == 1 and a == 0:
+    print("Jogador 1 ganha!")
+
+elif r == 0 and a == 1:
+    print("Jogador 1 ganha!")
+
+elif r == 0 and a == 0:
+
+    if p == 1 and result % 2 == 0:
+        print("Jogador 1 ganha!")
+
+    elif p == 0 and result % 2 == 1:
+        print("Jogador 1 ganha!")
+
+    else:
+        print("Jogador 2 ganha!")
