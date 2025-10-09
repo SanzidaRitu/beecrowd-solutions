4228e3dbafed778e8080476deb32555639db68d3
SanzidaRitu <mail.sanzida@gmail.com>
2025-10-09 15:13:30 +0600

    Added Problem No 1564

---
+++ 1564 - Brazil World Cup.py
@@ -0,0 +1,11 @@
+while True:
+    try:
+        N = int(input())
+
+        if N > 0:
+            print("vai ter duas!")
+        else:
+            print("vai ter copa!")
+
+    except EOFError:
+        break
