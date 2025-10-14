203777a4caaa457d8acf98c36898321294644b51
SanzidaRitu <mail.sanzida@gmail.com>
2025-10-14 12:18:29 +0600

    Added Problem No 2483

---
+++ 2483 - Merry Christmaaas!.py
@@ -0,0 +1,13 @@
+I = int(input())
+
+char_1 = "Feliz"
+char_2 = " "
+char_3 = "nat"
+char_4 = ""
+char_5 = "l!"
+
+for i in range(I):
+    char_4 += "a"
+
+result = char_1 + char_2 + char_3 + char_4 + char_5
+print(result)
