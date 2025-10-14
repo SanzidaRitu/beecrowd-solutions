e97163ea5523c0a1ddd47dd8b15cf5a9b7cc7cae
SanzidaRitu <mail.sanzida@gmail.com>
2025-10-14 18:35:01 +0600

    Added Problem No 2520

---
+++ 2520 - The Last Analógimôn.py
@@ -0,0 +1,32 @@
+while True:
+    try:
+        M, N = map(int, input().split())
+        matrix = []
+
+        for i in range(M):
+            row = []
+            row = input().split()
+            row = [int(char) for char in row]
+            matrix.append(row)
+
+        pos_1 = []
+        pos_2 = []
+
+        for r in range(M):
+            for c in range(N):
+                if matrix[r][c] == 1:
+                    pos_1.append(r)
+                    pos_1.append(c)
+
+                if matrix[r][c] == 2:
+                    pos_2.append(r)
+                    pos_2.append(c)
+
+        x_dist = abs(pos_1[0] - pos_2[0])
+        y_dist = abs(pos_1[1] - pos_2[1])
+
+        time = x_dist + y_dist
+        print(time)
+
+    except EOFError:
+        break
