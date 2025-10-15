59b0a3f2123b384b86a6ec32f947443c255ebd9b
SanzidaRitu <mail.sanzida@gmail.com>
2025-10-15 14:01:04 +0600

    Added Problem No 2540

---
+++ 2540 - Leader's Impeachment.py
@@ -0,0 +1,17 @@
+while True:
+    try:
+        N = int(input())
+
+        poll = list(map(int, input().split()))
+        t_vote = sum(poll)
+
+        yes = (2 / 3) * N
+
+        if t_vote >= yes:
+            print('impeachment')
+
+        else:
+            print("acusacao arquivada")
+
+    except EOFError:
+        break
