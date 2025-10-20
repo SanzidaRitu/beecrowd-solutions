8d72906f2ab469cebb683a129fd201f1b7aeb4e4
SanzidaRitu <mail.sanzida@gmail.com>
2025-10-20 14:51:42 +0600

    Added Problem No 2708

---
+++ 2708 - Tourists in the Huacachina Park.py
@@ -0,0 +1,19 @@
+tourist = 0
+jeep = 0
+
+while True:
+    user_input = input().split()
+
+    if user_input[0] == "ABEND":
+        break
+
+    if user_input[0] == "SALIDA":
+        jeep += 1
+        tourist += int(user_input[1])
+
+    if user_input[0] == "VUELTA":
+        jeep -= 1
+        tourist -= int(user_input[1])
+
+print(tourist)
+print(jeep)
