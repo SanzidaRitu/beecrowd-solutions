0bbf2bce4bc1e880c6d397c4f0b102006a0549ed
SanzidaRitu <mail.sanzida@gmail.com>
2025-10-22 15:40:41 +0600

    Added Problem No 2747

---
+++ 2747 - Output 1.py
@@ -0,0 +1,19 @@
+def dash():
+    dash_p = ""
+
+    for i in range(39):
+        dash_p += "-"
+    print(dash_p)
+
+def line():
+    for i in range(5):
+        line_p = "|"
+
+        for i in range(37):
+            line_p += " "
+        line_p += "|"
+        print(line_p)
+
+dash()
+line()
+dash()
