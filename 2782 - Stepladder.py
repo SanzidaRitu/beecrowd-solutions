b652bd7da3f807bf02dd0d9fb2b68cc12fa6b23d
SanzidaRitu <mail.sanzida@gmail.com>
2025-10-25 18:27:03 +0600

    Added Problem No 2782

---
+++ 2782 - Stepladder.py
@@ -0,0 +1,13 @@
+N = int(input())
+sequence = list(map(int, input().split()))
+result = 1
+
+if N == 1 or N == 2:
+    print(1)
+
+else:
+    for i in range(2, N):
+        if sequence[i] - sequence[i-1] != sequence[i-1] - sequence[i-2]:
+            result += 1
+
+    print(result)
