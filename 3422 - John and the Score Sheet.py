fe1bfd088fa19274801479e7a3fad9dc9ee68347
SanzidaRitu <mail.sanzida@gmail.com>
2025-12-17 17:55:53 +0600

    Added Problem No 3422(AD-HOC)

---
+++ 3422 - John and the Score Sheet.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+for i in range(n):
+    in_arr = input().split(" ")
+    t = int(in_arr[0])
+    term = int(in_arr[1].replace("T", ""))
+    start_time = (term - 1) * 45
+
+    if t > 45:
+        answer = "{term_time}+{bonus}".format(term_time=start_time + 45, bonus=t - 45)
+    else:
+        answer = "{term_time}".format(term_time=start_time + t)
+    print(answer)
